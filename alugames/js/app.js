a126c882d67a9b83e54c07e6d5886f49a58ced9a
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2024-07-04 11:40:04 -0300

    Update app.js

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,22 +1,39 @@
 let botao;
 let imagem;
-let jogo;
+let jogoId;
+let nomeJogo;
+let jogosAlugados = 0;
+
 //Função para alterar o status de cada botão.
 function alterarStatus(id){  
-    jogo = document.getElementById(`game-${id}`);
-    imagem = jogo.querySelector('.dashboard__item__img');
-    botao = jogo.querySelector('.dashboard__item__button');
-
+    jogoId = document.getElementById(`game-${id}`);
+    imagem = jogoId.querySelector('.dashboard__item__img');
+    botao = jogoId.querySelector('.dashboard__item__button');
+    nomeJogo = jogoId.querySelector('.dashboard__item__name').textContent;
     if(imagem.classList.contains('dashboard__item__img--rented')){
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
 
-    }else{
+        if(confirm(`Você quer devolver o jogo: ${nomeJogo}?`)){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
+    }else{ 
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        jogosAlugados++;
 
     }
-     
+     exibirJogosAlugados();
 }
+
+//Esse código irá fazer com que o DOM do HTML seja carregado, já emitindo a quantidade de jogos alugados sem ter feito antes alguma alteração.
+function exibirJogosAlugados(){
+    console.log(`Jogos alugados: ${jogosAlugados}`)
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    exibirJogosAlugados();
+})
